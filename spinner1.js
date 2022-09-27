2d84d88f4f585cb4d16ba74ec5245185f2c319df
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-27 18:10:30 +0000

    Completed spinner exercise

--- spinner1.js
+++ spinner1.js
@@ -1,16 +1,16 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+let spinChars = ['|', '/', '-', '\\'];
+let delay = 0;
+let spins = 5;
+
+for (let i = 0; i < spins; i++) {
+  for (const char of spinChars) {
+    setTimeout(() => {
+      process.stdout.write(`\r${char}   `);
+    }, delay);
+    delay += 100;
+  }
+}
 
 setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
+  console.log();
+}, delay);
